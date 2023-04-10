b881a4a5c6a16ccafb8026802092e28db627b288
ksandrsl <ksandr.sl@gmail.com>
2023-04-10 16:11:31 +0300

    fix: refactor burger menu

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -7,56 +7,38 @@ burgerBtn.addEventListener('click', (e) => {
     //останавливаем всплытие события
     e.stopPropagation();
 
-    burgerMenu.style.display = 'flex';
-    setTimeout(() => {
-        burgerMenu.classList.toggle('_show');
-    });
-    if (burgerBtn.classList.contains('_open')) {
-        setTimeout(() => {
-            burgerMenu.style.display = 'none';
-        }, 100);
-    };
+    //блокируем скролл на странице
+    document.documentElement.style.overflow = 'hidden';
 
+    //показываем/скрываем кнопку
     burgerBtn.classList.toggle('_open');
+    //показываем/скрываем меню
+    burgerMenu.classList.toggle('_show');
+    //показываем/скрываем фон
     blackout.classList.toggle('visible');
 
-    document.documentElement.style.overflow = 'hidden'
-
-    console.log('open? ' + burgerBtn.classList.contains('_open'));
-
-    setTimeout(() => {
-        if (!burgerBtn.classList.contains('_open')) document.documentElement.style.overflow = '';
-    });
-
+    //ЕСЛИ меню скрыто включаем скролл
+    if (!burgerMenu.classList.contains('_show')) document.documentElement.style.overflow = '';
+    //ЕСЛИ фон свелый переключаем цвет кнопки
+    if (burger.classList.contains('burger_lite')) burgerBtn.classList.toggle('_lite');
 });
-
-if (burger.classList.contains('burger_lite')) {
-
-    burgerBtn.addEventListener('click', (e) => {
-        burgerBtn.classList.toggle('_lite');
-    });
-}
 
 document.addEventListener('click', (e) => {
 
-    //ЕСЛИ клик НЕ по указанному блоку ИЛИ по элементу с указанным классом...
     //composedPath() возвращает путь к событию в виде массива объектов
     //includes() проверяет наличие элемента в массиве
     if (!e.composedPath().includes(burgerMenu) || e.target.classList.contains('menu__link')) {
 
-        burgerMenu.classList.remove('_show');
-
-        setTimeout(() => {
-            burgerMenu.style.display = 'none';
-        }, 100);
-
+        //ЕСЛИ клик НЕ по указанному блоку ИЛИ по элементу с указанным классом...
+        //скрываем кнопку
         burgerBtn.classList.remove('_open');
+        //скрываем меню
+        burgerMenu.classList.remove('_show');
+        //скрываем фон
         blackout.classList.remove('visible');
+        //включаем скролл
         document.documentElement.style.overflow = '';
-
-        if (burger.classList.contains('burger_lite')) {
-            burgerBtn.classList.add('_lite');
-        }
-
+        //ЕСЛИ фон свелый красим кнопку
+        if (burger.classList.contains('burger_lite')) burgerBtn.classList.add('_lite');
     }
 });
